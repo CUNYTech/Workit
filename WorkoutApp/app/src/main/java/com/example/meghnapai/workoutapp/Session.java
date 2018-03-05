53539adb185e8b67c7b3e298dbf1e62cd97403b0
Brandon Golden <brandon.golden55@myhunter.cuny.edu>
2018-03-05 15:27:34 -0500

    Minor touch-up of the Session class.

--- WorkoutApp/app/src/main/java/com/example/meghnapai/workoutapp/Session.java
+++ WorkoutApp/app/src/main/java/com/example/meghnapai/workoutapp/Session.java
@@ -4,9 +4,9 @@ import android.content.SharedPreferences;
 import android.content.Context;
 
 public class Session {
-    public static final String Username = "username";
-    public static final String Password = "password";
-    public static final String PREFERENCES_NAME = "WorkoutApp";
+    private static final String Username = "username";
+    private static final String Password = "password";
+    private static final String PREFERENCES_NAME = "WorkoutApp";
     SharedPreferences prefs;
     SharedPreferences.Editor editor;
 
@@ -18,22 +18,22 @@ public class Session {
     }
 
     public void setUsername(String username) {
-        editor.putString("username", username);
+        editor.putString(Username, username);
         editor.commit();
     }
 
     public String getUsername() {
-        String username = prefs.getString("username","");
+        String username = prefs.getString(Username, "");
         return username;
     }
 
     public void setPassword(String password) {
-        editor.putString("password", password);
+        editor.putString(Password, password);
         editor.commit();
     }
 
     public String getPassword() {
-        String password = prefs.getString("password","");
+        String password = prefs.getString(Password, "");
         return password;
     }
 }
