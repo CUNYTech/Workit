681894982d0f5bb7b9b8473df2a2f24f7cfe406c
russ226 <russ2510@gmail.com>
2018-04-25 16:37:09 -0400

    stuff

--- AndroidFrontend/app/src/main/java/com/example/meghnapai/workoutapp/HomeFragment.java
+++ AndroidFrontend/app/src/main/java/com/example/meghnapai/workoutapp/HomeFragment.java
@@ -1,14 +1,14 @@
 package com.example.meghnapai.workoutapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
+import android.widget.Button;
 
 
 public class HomeFragment extends Fragment {
@@ -27,7 +27,21 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        View v= inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button chat = (Button) v.findViewById(R.id.IncDist);
+
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent schedule = new Intent(getActivity(), CalendarActivity.class);
+                startActivity(schedule);
+            }
+        });
+
+        return v;
     }
 
 
